69df27b63bb9d1bdee35ee07b56e9a469d28d18f
k0001 <gnuk0001@gmail.com>
2009-06-11 16:00:31 -0300

    distutils setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from distutils.core import setup
+
+
+setup(name='meaningtoolws',
+      version='0.1',
+      description='Meaningtool Web Services Python Client',
+      author='Popego Corporation',
+      author_email='contact@popego.com',
+      url='http://github.com/k0001/meaningtoolws',
+      packages=['meaningtoolws'],
+     )
+
